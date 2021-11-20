f092a2cdfb03a15ed1f25de3a55f8ee7513d6ac7
yuki1923 <kygarcons0629@gmail.com>
2021-11-21 08:47:40 +0900

    bladeのroute()内に半角スペース追加

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -16,10 +16,10 @@
         </p>
         <div class="text-right">
           @if($post->user_id === Auth::id())
-          <a href="{{ route('post.edit',['id' => $post->id]) }}" class="btn btn-success btn-sm">
+          <a href="{{ route('post.edit', ['id' => $post->id]) }}" class="btn btn-success btn-sm">
             <i class="far fa-edit"></i>編集
           </a>
-          <a href="{{ route('post.delete',['id' => $post->id]) }}" class="btn btn-danger btn-sm" rel="nofollow">
+          <a href="{{ route('post.delete', ['id' => $post->id]) }}" class="btn btn-danger btn-sm" rel="nofollow">
             <i class="far fa-trash-alt"></i>削除
           </a>
           @endif
